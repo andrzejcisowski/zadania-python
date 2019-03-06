2118982eec29e2239e8d41cb6ef5a61f8eebf682
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-06 21:20:23 +0100

    zadania

---
+++ temperatura.py
@@ -0,0 +1,16 @@
+# - user - \\
+# - UTF -8 - \\
+# - wprowadzamy dane \\
+
+print('Wybierz jednostkę temperatury "c" lub "f" i naciśnij ENTER:')
+j = str(input())
+t = ("podaj temperaturę:")
+if j == 'f' or j == 'c':
+    print('Podaj temperaturę:')
+    t = input()
+    if j == 'c' and t.isdigit():
+        print(int(t) * 1.8 + 32,  'F')
+    elif j == 'f' and t.isdigit():
+        print((int(t) - 32) / 1.8, 'C')
+else:
+    print('Błędne dane')
