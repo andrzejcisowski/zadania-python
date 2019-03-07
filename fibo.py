a36a213d08614fd8ba4cb9205305d2fa23e03ae6
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-07 23:27:42 +0100

    fibo

---
+++ fibo.py
@@ -0,0 +1,11 @@
+# ACDell\user\desktop\\
+# -UTF - 8 \\
+
+a, b = (0, 1)    #start - cyfry pierwsze w ciągu\\
+n = 200          # ograniczenie zmienna
+while b < n:
+    a, b = b, a + b
+    print(b)
+
+
+
