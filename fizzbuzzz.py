f95d079db804dda181a1074ad6ba3e741388da0d
andrzejcisowski <47793537+andrzejcisowski@users.noreply.github.com>
2019-03-08 21:33:44 +0100

    Update fizzbuzzz.py

--- fizzbuzzz.py
+++ fizzbuzzz.py
@@ -1,6 +1,5 @@
 # ACDell\pulpit\zadania1\\
 # - UTC-8 \\
-range(101)
 print('"NIE DZIELIMY ZERA"')            # dla 0 zwróci fizz,buzz,FizzBuzz\\
 for liczba in range(1, 101):            # wartość  w przedziale 1-100\\
     if liczba % 3 == 0:               # liczba podzielna przez 3 bez reszty\\
