281603555466c7133a15ba9f9b334cb11747a2c6
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-16 21:22:11 +0100

    zadania2

---
+++ week2.exercises2/samogłoski i spółgłoski.py
@@ -0,0 +1,14 @@
+#ACDell\pulpit\zadania \\
+# - UTC - 8
+#liczba samogłosek i spółgłosek
+
+samogloski = 0
+spolgloski = 0
+print('wpisz słowo')
+
+for litera in input():
+    if litera.lower() in ('aeioyu'):
+        samogloski = samogloski + 1
+    else:
+        spolgloski += 1
+print(f"Samoglosek: {samogloski}, spolglosek {spolgloski}")
