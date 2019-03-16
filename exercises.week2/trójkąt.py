a12f435437733a6c78d8747b8286279363dfe6ee
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-16 21:36:48 +0100

    zadania2

--- exercises.week2/trójkąt.py
+++ exercises.week2/trójkąt.py
@@ -1,12 +1,12 @@
 # ACDell\pulpit\zadania1\\
 # - UTC-8 \\
-a = int(input ("Podaj bok a: "))
+a = int(input("Podaj bok a: "))
 print(a)
 b = int(input("Podaj bok b: "))
 print(b)
 c = int(input("Podaj bok c: "))
 print(c)
 if c ** 2 == a ** 2 + b ** 2:
-    print(' Z podanych danych zbudujesz trójkąt pitagorejski')
+    print('Z podanych danych zbudujesz trójkąt pitagorejski, o przeciwprostokątnej =', c ** 2)
 else:
     print('Z podanych danych nie zbudujesz trójkąta pitagorejskiego')
--- week2.exercises2/choinka.py
+++ week2.exercises2/choinka.py
@@ -1,4 +1,4 @@
 # - * UTC - 8\\
-for a in range(0, 5):
-    for b in range(1, 8):
+for a in range(1, 5):
+    for b in range(1, 10):
         print(b * "#")
