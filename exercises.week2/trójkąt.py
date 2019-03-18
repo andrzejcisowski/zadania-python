f91e5e5bf567223c3af3db5373bdb4d28db10ad5
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-18 21:12:29 +0100

    zadania2

--- exercises.week2/trójkąt.py
+++ exercises.week2/trójkąt.py
@@ -1,12 +1,12 @@
 # ACDell\pulpit\zadania1\\
 # - UTC-8 \\
 a = int(input("Podaj bok a: "))
-
 b = int(input("Podaj bok b: "))
-
 c = int(input("Podaj bok c: "))
 
 if c ** 2 == a ** 2 + b ** 2:
     print('Z podanych danych zbudujesz trójkąt pitagorejski, o przeciwprostokątnej =', c ** 2)
+    if a == 3 and b == 4 and c == 5:
+        print('Brawo, z wprowadzonych danych zbudujesz trójkąt egipski')
 else:
-    print('Z podanych danych nie zbudujesz trójkąta pitagorejskiego')
+        print('Z podanych danych nie zbudujesz trójkąta pitagorejskiego')
