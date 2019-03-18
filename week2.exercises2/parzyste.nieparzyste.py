69fe2dfe7199af6703de57194fbfbad869495724
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-18 23:06:55 +0100

    zadania2

--- week2.exercises2/parzyste.nieparzyste.py
+++ week2.exercises2/parzyste.nieparzyste.py
@@ -15,10 +15,13 @@ print("parzyste: {}), nieparzyste :{}".format(a, b))
 # wersja 2
 a = 0
 b = 0
-podaj_zakres = int(input('Podaj zakres :'))
-for liczba in range(int(podaj_zakres)):
-    if liczba % 2 != 0:
-        a = a+1
-    else:
-        b = b+1
-print("parzyste : {}, nieparzyste :{}".format(a, b))
+podaj_zakres = (input('Podaj zakres :'))
+if podaj_zakres.isdigit():
+    for liczba in range(int(podaj_zakres)):
+        if liczba % 2 != 0:
+            a += 1
+        elif liczba % 2 == 0:
+            b += 1
+    print("parzyste : {}, nieparzyste :{}".format(a, b))
+else:
+    print('Niepoprawne dane , wprowadź cyfry :')
