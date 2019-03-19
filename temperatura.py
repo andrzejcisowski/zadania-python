c44140495d239bf622b706cf545a8ea9d457cdb6
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-19 21:52:53 +0100

    zadania2

--- temperatura.py
+++ temperatura.py
@@ -5,12 +5,15 @@
 print('Wybierz jednostkę temperatury "c" lub "f" i naciśnij ENTER:')
 j = str(input())
 t = ("podaj temperaturę:")
+j = j.lower()                              # zamienia wejściowe litery  małe ( nieważna jest wielkość liter)
 if j == 'f' or j == 'c':
     print('Podaj temperaturę:')
     t = input()
     if j == 'c' and t.isdigit():
-        print(int(t) * 1.8 + 32,  'F')
-    if j == 'f' and t.isdigit():
-        print((int(t) - 32) / 1.8, 'C')
+        print('Podaję temperaturę w stopniach Fahrenheit :', int(t) * 1.8 + 32,  'F')
+        if j == 'f' and t.isdigit():
+            print('Podaję temperaturę w stopniach Celcjusz :', (int(t) - 32) / 1.8, 'C')
     else:
         print('Błędne dane')
+else:
+    print('Błędne dane')
