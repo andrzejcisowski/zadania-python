b5a41d65f9450ffb32f79203ba53c7741c344a12
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-21 16:54:49 +0100

    zadania3

--- week3.exercise2/liczba.pierwsza.py
+++ week3.exercise2/liczba.pierwsza.py
@@ -3,11 +3,11 @@
 
 def libp(pierw):
     for a in range(2, pierw):
-        if pierw % a == 0:  # można dodac and a!=1
+        if pierw % a == 0: #można dodać warunek and a!=1:
             print(pierw, "nie jest liczba pierwsza")
             break
     else:
         print(pierw, 'jest liczbą pierwszą')
 
+print (libp(int(input('podaj liczbe: '))))
 
-libp(89)
