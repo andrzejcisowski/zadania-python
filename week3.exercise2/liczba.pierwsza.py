8fc88af5189d14c572b417df5d2d58d260c569a0
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-21 22:47:21 +0100

    dania3

--- week3.exercise2/liczba.pierwsza.py
+++ week3.exercise2/liczba.pierwsza.py
@@ -1,13 +1,27 @@
 #ACDell\desktop \\
 #  - UTC 8 -   \\
+# sposób 1
 
-def libp(pierw):
+nr = int(input("Podaj liczbe "))
+
+if nr >= 1:
+    for i in range(2, nr):
+        if (nr % i) == 0:
+            print(nr, "nie jest liczbą pierwszą")
+            print(i, "razy", nr // i, "jest", nr)
+            break
+    else:
+        print(nr, "to jest liczba pierwsza")
+
+#sposób z funkcją
+
+"""def libp(pierw):
+
     for a in range(2, pierw):
         if pierw % a == 0: #można dodać warunek and a!=1:
             print(pierw, "nie jest liczba pierwsza")
             break
     else:
         print(pierw, 'jest liczbą pierwszą')
-
-print (libp(int(input('podaj liczbe: '))))
-
+        #return libp(int(input('podaj liczbe: ')))
+libp(9)"""
