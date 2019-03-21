d00a427181976e2004431d8fbdf9c276e7705931
andrzej.cisowski <andrzej.cisowski@com-4t.pl>
2019-03-21 23:09:30 +0100

    zadania2

--- week2.exercises2/ciąg fibbon.py
+++ week2.exercises2/ciąg fibbon.py
@@ -2,9 +2,13 @@
 # -UTF - 8 \\
 
 a, b = (0, 1)    #start - cyfry pierwsze w ciągu\\
-n = 200          # ograniczenie zmienna
+n = int(input("Podaj zakres, do jakiego obliczymy ciąg  : "))
 while b < n:
-    a, b = b, a + b
+    if n > 10000:                              # ograniczenie przed zapętleniem - wykonanie do 10000\\
+        print('przekroczyłeś zakres')
+        break
+    else:
+        a, b = b, a + b
     print(b)
 
 
